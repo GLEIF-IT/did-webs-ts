ac09bb5c27b885efff284d44c75f2b3ba9dd0a62
Jonathan Rayback <jrayback@gmail.com>
2025-03-12 15:41:10 -0600

    fix path problem in compatibility check scripts; make their output look nicer
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- build/test/package/node/esm/script.js
+++ build/test/package/node/esm/script.js
@@ -2,14 +2,15 @@ import * as messages from 'did-webs-from-vlei';
 
 import { sayHello, sayGoodbye } from 'did-webs-from-vlei';
 
-console.log('\n[esm]');
+console.log('\n\x1b[34m**************');
+console.log('* EcmaScript *');
+console.log('**************\x1b[0m\n\n');
 
 console.log(
-  messages.sayHello() === 'hello!' && messages.sayGoodbye() === 'goodbye!'
-    ? 'pass'
-    : 'fail'
-);
-
-console.log(
-  sayHello() === 'hello!' && sayGoodbye() === 'goodbye!' ? 'pass' : 'fail'
+  messages.sayHello() === 'hello!' &&
+    messages.sayGoodbye() === 'goodbye!' &&
+    sayHello() === 'hello!' &&
+    sayGoodbye() === 'goodbye!'
+    ? '\x1b[32m[x] OK\x1b[0m'
+    : '\x1b[31m[x] FAIL\x1b[0m'
 );
