4f3ca107b682d7cecb8abecf2f43f3b3cc37f34b
Jonathan Rayback <jrayback@gmail.com>
2025-03-12 15:41:10 -0600

    fix path problem in compatibility check scripts; make their output look nicer
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- build/test/package/checkPackage.sh
+++ build/test/package/checkPackage.sh
@@ -4,24 +4,31 @@
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 runNodeTest() {
-    echo "\n[node.js]:\n"
+    printf "\n\e[34m***********\n"
+    printf "* Node.js *\n"
+    printf "***********\e[0m\n\n"
+    
     cd "$BASE_DIR/node" || { echo "Directory not found"; exit 1; }
     rm -rf node_modules
     npm run node-test || { echo "Node test failed"; exit 1; }
 }
 
 runWebTest() {
-    echo "\n\n[web]:\n"
-    echo "checking..."
+    printf "\n\e[34m*******\n"
+    printf "* Web *\n"
+    printf "*******\e[0m\n\n"
+  
     cd "$BASE_DIR/web" || { echo "Directory not found"; exit 1; }
     rm -rf node_modules
     npm install || { echo "npm install failed"; exit 1; }
     npm run web-test || { echo "Web test failed"; exit 1; }
-    echo "\n...open index.html in a browser to view results...\n"
+    printf "\n\e[33m[?] OK\e[0m (Open 'index.html' in a browser to view results)\n"
 }
 
 # Ensure we always start from the base directory
 cd "$BASE_DIR" || { echo "Failed to navigate to base directory"; exit 1; }
 
+echo "Checking compatibility..."
 runNodeTest
 runWebTest
+echo ""
--- build/test/package/node/cjs/script.js
+++ build/test/package/node/cjs/script.js
@@ -1,9 +1,11 @@
 const messages = require('did-webs-from-vlei');
 
-console.log('\n[cjs]');
+console.log('\n\x1b[34m************');
+console.log('* CommonJS *');
+console.log('************\x1b[0m\n\n');
 
 console.log(
   messages.sayHello() === 'hello!' && messages.sayGoodbye() === 'goodbye!'
-    ? 'pass'
-    : 'fail'
+    ? '\x1b[32m[x] OK\x1b[0m'
+    : '\x1b[31m[x] FAIL\x1b[0m'
 );
