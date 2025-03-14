a8fe84b85aa2eb907dfd3cdc610b4a6faa5268e9
Jonathan Rayback <jrayback@gmail.com>
2025-03-14 08:38:00 -0600

    set up custome step runner flow; make tests all pass
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import { hello, goodbye } from './salutation/salutation.js';
-
-export function sayHello(): string {
-  const message = `${hello}!`;
-  return message;
-}
-
-export function sayGoodbye(): string {
-  const message = `${goodbye}!`;
-  return message;
-}
-
-export default {
-  sayHello,
-  sayGoodbye,
-};
+// api not implemented yet
---
+++ src/runners/happyRunner.test.ts
@@ -0,0 +1,34 @@
+import { jest } from '@jest/globals';
+import { StepRunner } from '@gleif-it/vlei-verifier-workflows';
+import { HappyRunner } from './happyRunner.js';
+
+describe('happyRunner', () => {
+  const myHappyRunner = new HappyRunner();
+  it('should exist', () => {
+    expect(typeof HappyRunner).toBe('function');
+  });
+  it('should be a StepRunner', () => {
+    expect(myHappyRunner instanceof StepRunner).toBe(true);
+  });
+  it('should have a run method', () => {
+    expect(typeof myHappyRunner.run).toBe('function');
+  });
+  it('should return a successful result', async () => {
+    const result = await myHappyRunner.run({}, 'myStep', {});
+    expect(result.success).toBe(true);
+  });
+  it('should log the expected message to the console', async () => {
+    // Mock console.log using Jest with an empty function
+    const logMock = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+    // Run the function
+    await myHappyRunner.run(null, '', {});
+
+    // Check if console.log was called with expected message
+    expect(logMock).toHaveBeenCalledTimes(1);
+    expect(logMock).toHaveBeenCalledWith("I'm happy! Are you happy?");
+
+    // Restore the original console.log
+    logMock.mockRestore();
+  });
+});
