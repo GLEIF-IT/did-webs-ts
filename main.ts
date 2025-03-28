e9d4b832be66adb76d80bae2bd8078f3019a7d3f
Jonathan Rayback <jrayback@gmail.com>
2025-03-27 21:30:12 -0600

    remove reference to vlei-verifier-workflow
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- main.ts
+++ main.ts
@@ -1,20 +1,2 @@
 // THIS FILE IS USED BY npm start; IT'S JUST A MECHANISM TO RUN PACKAGE INTERNALS; DOES NOT SHIP WITH THE PACKAGE
-import {
-  WorkflowRunner,
-  getConfig,
-  loadWorkflow,
-} from '@gleif-it/vlei-verifier-workflows';
-
-import { DidWebsRunner } from './src/runners/didWebsRunner.js';
-
-export const runWorkflow = async () => {
-  const workflow = loadWorkflow('src/workflows/workflow.yaml');
-  const configJson = getConfig('src/configs/configuration.json');
-  const runner = new WorkflowRunner(workflow, configJson);
-  runner.registerRunner('did-webs', new DidWebsRunner());
-  return await runner.runWorkflow();
-};
-
-runWorkflow().then((result) => {
-  console.log(result);
-});
+console.log('vlei-to-did-webs');
