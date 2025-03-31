3c1983dc6af97c78e3b1691508abe93f50d3b278
Jonathan Rayback <jrayback@gmail.com>
2025-03-30 23:03:03 -0600

    reorganize credential types directory
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- src/core/credential/Credential.ts
+++ src/core/credential/Credential.ts
@@ -1,9 +1,9 @@
-import { ECRAuthorizationvLEICredential } from './ECRAuthorizationvLEICredential.js';
-import { ECRvLEICredential } from './ECRvLEICredential.js';
-import { LegalEntityvLEICredential } from './LegalEntityvLEICredential.js';
-import { OORAuthorizationvLEICredential } from './OORAuthorizationvLEICredential.js';
-import { OORvLEICredential } from './OORvLEICredential.js';
-import { QVIvLEICredential } from './QVIvLEICredential.js';
+import { ECRAuthorizationvLEICredential } from './types/ECRAuthorizationvLEICredential.js';
+import { ECRvLEICredential } from './types/ECRvLEICredential.js';
+import { LegalEntityvLEICredential } from './types/LegalEntityvLEICredential.js';
+import { OORAuthorizationvLEICredential } from './types/OORAuthorizationvLEICredential.js';
+import { OORvLEICredential } from './types/OORvLEICredential.js';
+import { QVIvLEICredential } from './types/QVIvLEICredential.js';
 
 export type Credential =
   | ECRAuthorizationvLEICredential
