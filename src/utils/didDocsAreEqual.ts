a90c53d81e354708f83d0c82049f5c46d798625c
Jonathan Rayback <jrayback@gmail.com>
2025-04-02 12:18:55 -0600

    update validators
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- src/utils/didDocsAreEqual.ts
+++ src/utils/didDocsAreEqual.ts
@@ -10,5 +10,7 @@ export const didDocsAreEqual = (doc1: object, doc2: object): boolean => {
     .createHash('sha256')
     .update(stringify(doc2) as string)
     .digest('hex');
+  console.log(`comparing this: ${value1}`);
+  console.log(`to that:        ${value2}`);
   return value1 === value2;
 };
--- src/utils/validators/isValidDidWebs.ts
+++ src/utils/validators/isValidDidWebs.ts
@@ -1,7 +1,7 @@
 export const isValidDidWebs = (input: string): boolean =>
   new RegExp(
     '^did:webs:' +
-      '((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\\.(?:[a-zA-Z]{2,63}|xn--[a-zA-Z0-9]+))' + // Legal hostname as per RFC 1035 & 1123
+      '((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\\.(?:[a-zA-Z]{2,63}|xn--[a-zA-Z0-9]+))' + // Legal hostname as per RFC 1035 & 1123 -> DOES NOT SUPPORT 'localhost', etc.
       '(?:%3A(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|6553[0-5]))?' + // Valid port range: 0-65535
       "(?::([a-zA-Z0-9-._~!$&'()*+,;=]+))*" + // Zero or more path segments
       ':(E[A-Za-z0-9-_]{43})$' // AID must start with 'E', be Base64URL, and be exactly 44 characters long
