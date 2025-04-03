e9d88283a19b9ed64ecc4bd560883310731e9c79
Jonathan Rayback <jrayback@gmail.com>
2025-04-02 23:03:16 -0600

    finish multi sig no delegate
    Signed-off-by: Jonathan Rayback <jrayback@gmail.com>

--- test/data/examples/didDocs/multiSigNoDelegate/multi-sig-no-delegate-Ew-o.ts
+++ test/data/examples/didDocs/multiSigNoDelegate/multi-sig-no-delegate-Ew-o.ts
@@ -17,39 +17,39 @@ export default {
       id: '#1AAAAg299p5IMvuw71HW_TlbzGq5cVOQ7bRbeDuhheF-DPYk',
       type: 'JsonWebKey',
       controller:
-        'did:webs:example.com:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
+        'did:webs:example.com%3A8080:path:to:dids:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
       publicKeyJwk: {
         kid: '1AAAAg299p5IMvuw71HW_TlbzGq5cVOQ7bRbeDuhheF-DPYk',
         kty: 'EC',
         crv: 'secp256k1',
-        x: 'NtngWpJUr-rlNNbs0u-Aa8e16OwSJu6UiFf0Rdo1oJ4',
-        y: 'qN1jKupJlFsPFc1UkWinqljv4YE0mq_Ickwnjgasvmo',
+        x: 'Db32nkgy-7DvUdb9OVvMarlxU5DttFt4O6GF4X4M9iQ',
+        y: 'w5MifYMEP1lulhw2Ov8Vk_W_pg71WsAF8QodavOFxK4',
       },
     },
-    // {
-    //   id: '#DA-vW9ynSkvOWv5e7idtikLANdS6pGO2IHJy7v0rypvE',
-    //   type: 'JsonWebKey',
-    //   controller:
-    //     'did:webs:example.com:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
-    //   publicKeyJwk: {
-    //     kid: 'DA-vW9ynSkvOWv5e7idtikLANdS6pGO2IHJy7v0rypvE',
-    //     kty: 'OKP',
-    //     crv: 'Ed25519',
-    //     x: 'A-vW9ynSkvOWv5e7idtikLANdS6pGO2IHJy7v0rypvE',
-    //   },
-    // },
-    // {
-    //   id: '#DLWJrsKIHrrn1Q1jy2oEi8Bmv6aEcwuyIqgngVf2nNwu',
-    //   type: 'JsonWebKey',
-    //   controller:
-    //     'did:webs:example.com:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
-    //   publicKeyJwk: {
-    //     kid: 'DLWJrsKIHrrn1Q1jy2oEi8Bmv6aEcwuyIqgngVf2nNwu',
-    //     kty: 'OKP',
-    //     crv: 'Ed25519',
-    //     x: 'LWJrsKIHrrn1Q1jy2oEi8Bmv6aEcwuyIqgngVf2nNws',
-    //   },
-    // },
+    {
+      id: '#DA-vW9ynSkvOWv5e7idtikLANdS6pGO2IHJy7v0rypvE',
+      type: 'JsonWebKey',
+      controller:
+        'did:webs:example.com%3A8080:path:to:dids:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
+      publicKeyJwk: {
+        kid: 'DA-vW9ynSkvOWv5e7idtikLANdS6pGO2IHJy7v0rypvE',
+        kty: 'OKP',
+        crv: 'Ed25519',
+        x: 'D69b3KdKS85a_l7uJ22KQsA11LqkY7YgcnLu_SvKm8Q',
+      },
+    },
+    {
+      id: '#DLWJrsKIHrrn1Q1jy2oEi8Bmv6aEcwuyIqgngVf2nNwu',
+      type: 'JsonWebKey',
+      controller:
+        'did:webs:example.com%3A8080:path:to:dids:Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M',
+      publicKeyJwk: {
+        kid: 'DLWJrsKIHrrn1Q1jy2oEi8Bmv6aEcwuyIqgngVf2nNwu',
+        kty: 'OKP',
+        crv: 'Ed25519',
+        x: 'tYmuwogeuufVDWPLagSLwGa_poRzC7IiqCeBV_ac3C4',
+      },
+    },
   ],
   authentication: ['#Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M'],
   assertionMethod: ['#Ew-o5dU5WjDrxDBK4b4HrF82_rYb6MX6xsegjq4n0Y7M'],
